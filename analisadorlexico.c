b849a5fcfad65d7694b555bb47a33608a05533b8
Lucas Rezende Bruno <42577310+Kapir00to@users.noreply.github.com>
2019-09-01 15:56:47 -0300

    3rd commit

--- analisadorlexico.c
+++ analisadorlexico.c
@@ -5,7 +5,7 @@
 
 #define MAX_STRINGS 10
 #define NUM_LEXEMAS_RESERVADAS 26
-#define NUM_LEXEMAS_SEPARADORES 14
+#define NUM_LEXEMAS_SEPARADORES 13
 #define NUM_LEXEMAS_OP_ARITMETICOS 12
 #define NUM_LEXEMAS_OP_LOGICOS 9
 
@@ -19,8 +19,8 @@
   desde que mantida sua fonte e seu autor. */
 
 typedef struct tok{
-    char *tipo;
-    char *valor;
+    char tipo[MAX_STRINGS];
+    char valor[MAX_STRINGS];
     int linha;
     int coluna;
 } token_type;
@@ -28,7 +28,7 @@ typedef struct tok{
 // regex para identificador: /^[a-zA-Z_][a-zA-Z0-9_]*$/
 char **tabela_simbolos;
 char palavras_reservadas[NUM_LEXEMAS_RESERVADAS][MAX_STRINGS] = {"int", "char", "float", "long", "include", "define", "struct", "return", "if", "break", "continue", "for", "while", "do", "switch", "case", "else", "typedef", "define", "NULL", "bool", "EOF", "void", "printf", "fopen", "FILE", "const"};
-char separadores[NUM_LEXEMAS_SEPARADORES][2] = {";","[","]",",","(",")", "{", "}","\n", " ", "\t", "//","#", "&"};
+char separadores[NUM_LEXEMAS_SEPARADORES][2] = {";","[","]",",","(",")", "{", "}","\n", " ", "\t","#", "&"};
 char operadores_aritmeticos[NUM_LEXEMAS_OP_ARITMETICOS][3] = {"+", "-", "*", "/", "++", "--", "%", "+=", "-=", "*=", "/=", "%="};
 char operadores_logicos[NUM_LEXEMAS_OP_LOGICOS][3] = {"||", "&&", "<", ">", "<=", ">=", "=", "==", "!="};
 
@@ -80,6 +80,9 @@ int busca(char* palavra, int tipo ){  //verifica se a palavra e do determinado t
 void lexico(){
   FILE *codigo_fonte, *sep, *reservadas, *operadores, *simbolos; 
   codigo_fonte = fopen("codigo.txt", "r");
+
+  if (codigo_fonte == NULL) printf("Arquivo nao encontrado\n");
+  
   sep = fopen("separadores.txt", "w");
   reservadas = fopen("reservadas.txt", "w");
   operadores = fopen("operadores.txt", "w");
@@ -91,7 +94,7 @@ void lexico(){
   char ch;
   token_type* lista_de_tokens;
 
-  while((ch = getchar()) != EOF){
+  while((ch = fgetc(codigo_fonte)) != EOF){
     char buffer[2];
     buffer[0] = ch;
     buffer[1] = '\0';
@@ -103,17 +106,83 @@ void lexico(){
       token->linha = linha;
       token->coluna = coluna; 
       fprintf(sep, "%s", buffer);
+      printf("Tipo: %s  Valor: %s  Linha: %d  Coluna: %d\n", token->tipo, token->valor, token->linha, token->coluna)
       lista_de_tokens[0] = malloc (sizeof(token_type));
       lista_de_tokens[0] = token;
     }
     else{
 
-      if (ch == '\''){
-        ch = getchar();
-        linha++;
-        if (ch == '\n')
-          printf("ERRO LEXICO EM (%d, %d), CARACTERE INVALIDO: %c", linha, coluna, ch);
+      if (ch == '\''){   //pegou aspa simples
+        ch = fgetc(codigo_fonte);
+        coluna++;
+        
+        if (ch == '\n'){    //aspas simples e depois pula linha
+          printf("ERRO LEXICO EM (%d, %d), PALAVRA INVALIDA: %c", linha++, coluna - 1, ch);
+          coluna = 0;
+        } 
+        else{
+          strcat(palavra, ch);
+          ch = fgetc(codigo_fonte);
+          coluna++;
+          
+          if (ch == '\''){    //aspas simples seguidas de aspas simples, char vazio
+            strcat(palavra, ch);
+            token_type token = malloc (sizeof(token_type));
+            token->tipo = "literal";
+            token->valor = "\'\'";   //caractere vazio
+            token->linha = linha;
+            token->coluna = coluna; 
+            fprintf(sep, "%s", buffer);
+            printf("Tipo: %s  Valor: %s  Linha: %d  Coluna: %d\n", token->tipo, token->valor, token->linha, token->coluna)
+         }
+         else{
+           strcat(palavra, ch);
+           ch = fgetc(codigo_fonte);
+           coluna++;
+           
+           if (ch == '\n'){
+             coluna = 0;
+             linha++;
+           }
 
+           if(ch != '\''){
+             strcat(palavra, ch);
+             
+             do {
+               ch = fgetc(codigo_fonte);
+               strcat(palavra, ch);
+               coluna++;
+               
+               if (ch == '\n'){
+                 linha++;
+                 coluna = 0;
+               }
+             } while (ch != '\'')
+            
+             printf("ERRO LÉXICO EM %d %d, PALAVRA INVALIDA: %s", linha, coluna, ch);
+           } else {
+             token_type token = malloc (sizeof(token_type));
+             token->tipo = "literal";
+             token->valor = "\'\'";   //caractere vazio
+             token->linha = linha;
+             token->coluna = coluna; 
+             fprintf(sep, "%s", buffer);
+             printf("Tipo: %s  Valor: %s  Linha: %d  Coluna: %d\n", token->tipo, token->valor, token->linha, token->coluna)
+           }
+         }
+         
+          /*palavra[0] = '\'';
+          palavra[1] = ch;
+          palavra[2] = '\'';
+          token_type token = malloc (sizeof(token_type));
+          token->tipo = "literal"
+          token->valor = palavra;
+          token->linha = linha;
+          token->coluna = coluna; 
+          fprintf(sep, "%s", buffer);
+          printf("Tipo: %s  Valor: %s  Linha: %d  Coluna: %d\n", token->tipo, token->valor, token->linha, token->coluna)
+          lista_de_tokens[0] = token;*/
+        }
       }
       
 
